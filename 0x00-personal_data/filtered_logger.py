508216a818dbadc3e216deea88c55463e7db50ba
zeusgolohor <zeusgolohor@gmail.com>
2024-05-31 01:29:40 +0000

    refactor

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
-"""A module for filtering logs.
+"""
+A scriot to manage personal data.
 """
 import os
-import re
 import logging
-import mysql.connector
+import re
 from typing import List
+import mysql.connector
 
 
-patterns = {
+regx = {
     'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
     'replace': lambda x: r'\g<field>={}'.format(x),
 }
@@ -18,14 +19,18 @@ PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 def filter_datum(
         fields: List[str], redaction: str, message: str, separator: str,
         ) -> str:
-    """Filters a log line.
     """
-    extract, replace = (patterns["extract"], patterns["replace"])
+    A function called filter_datum that returns
+    the log message obfuscated.
+    """
+    extract, replace = (regx["extract"], regx["replace"])
     return re.sub(extract(fields, separator), replace(redaction), message)
 
 
 def get_logger() -> logging.Logger:
-    """Creates a new logger for user data.
+    """
+    Implement a get_logger function that takes no
+    arguments and returns a logging.Logger object.
     """
     logger = logging.getLogger("user_data")
     stream_handler = logging.StreamHandler()
@@ -37,7 +42,8 @@ def get_logger() -> logging.Logger:
 
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
-    """Creates a connector to a database.
+    """
+    A method to Connect to secure database.
     """
     db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
     db_name = os.getenv("PERSONAL_DATA_DB_NAME", "")
@@ -54,25 +60,27 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
 
 
 def main():
-    """Logs the information about user records in a table.
     """
-    fields = "name,email,phone,ssn,password,ip,last_login,user_agent"
-    columns = fields.split(',')
-    query = "SELECT {} FROM users;".format(fields)
-    info_logger = get_logger()
+    Implement a main function that takes no
+    arguments and returns nothing.
+    """
+    inputs = "name*email*phone*ssn*password*ip*last_login*user_agent"
+    columns = fields.split('*')
+    query = "SELECT {} FROM users;".format(inputs)
+    logger = get_logger()
     connection = get_db()
     with connection.cursor() as cursor:
         cursor.execute(query)
         rows = cursor.fetchall()
         for row in rows:
-            record = map(
+            res = map(
                 lambda x: '{}={}'.format(x[0], x[1]),
                 zip(columns, row),
             )
-            msg = '{};'.format('; '.join(list(record)))
-            args = ("user_data", logging.INFO, None, None, msg, None, None)
-            log_record = logging.LogRecord(*args)
-            info_logger.handle(log_record)
+            ms = '{};'.format('; '.join(list(res)))
+            ag = ("user_data", logging.INFO, None, None, ms, None, None)
+            log_record = logging.LogRecord(*ag)
+            logger.handle(log_record)
 
 
 class RedactingFormatter(logging.Formatter):
@@ -89,11 +97,12 @@ class RedactingFormatter(logging.Formatter):
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """formats a LogRecord.
         """
-        msg = super(RedactingFormatter, self).format(record)
-        txt = filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
-        return txt
+        A method used to format records.
+        """
+        ms = super(RedactingFormatter, self).format(record)
+        res = filter_datum(self.fields, self.REDACTION, ms, self.SEPARATOR)
+        return res
 
 
 if __name__ == "__main__":
