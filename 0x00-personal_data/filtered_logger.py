7df06ed19f7ca6f63af2aaf5d136d19c1e128f9d
zeusgolohor <zeusgolohor@gmail.com>
2024-05-31 00:01:25 +0000

    Copy the following code into filtered_logger.py.
    import logging
    
    class RedactingFormatter(logging.Formatter):
        """ Redacting Formatter class
            """
    
        REDACTION = "***"
        FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
        SEPARATOR = ";"
    
        def __init__(self):
            super(RedactingFormatter, self).__init__(self.FORMAT)
    
        def format(self, record: logging.LogRecord) -> str:
            NotImplementedError
    Update the class to accept a list of strings fields constructor argument.
    
    Implement the format method to filter values in incoming log records using filter_datum. Values for fields in fields should be filtered.
    
    DO NOT extrapolate FORMAT manually. The format method should be less than 5 lines long.

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,7 @@ A script called filter_datum that returns the log message obfuscated.
 """
 from typing import List
 import re
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -15,3 +16,27 @@ def filter_datum(fields: List[str], redaction: str,
         result = re.sub(f'{field}=.*?{separator}',
                         f'{field}={redaction}{separator}', message)
         return (result)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        A method to filter values in incoming log records
+        using filter_datum. Values for fields in fields
+        should be filtered.
+        """
+        return filter_datum(self.fields, self.REDACTION,
+                            record.getMessage(), self.SEPARATOR)
