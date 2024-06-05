77ddaf9dcb238dd26a0277d247f88b0bd2ea0e18
zeusgolohor <zeusgolohor@gmail.com>
2024-06-05 02:20:02 +0000

    Add the method def decode_base64_authorization_header(self, base64_authorization_header: str) -> str: in the class BasicAuth that returns the decoded value of a Base64 string base64_authorization_header:
    Return None if base64_authorization_header is None
    Return None if base64_authorization_header is not a string
    Return None if base64_authorization_header is not a valid Base64 - you can use try/except
    Otherwise, return the decoded value as UTF8 string - you can use decode('utf-8')

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 A module used to implement basic auth
 """
 from api.v1.auth.auth import Auth
+import base64
 from flask import request
 
 
@@ -23,3 +24,21 @@ class BasicAuth(Auth):
         if header.startswith("Basic "):
             return header[len("Basic "):]
         return None
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """
+        A method used to decode base64.
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            byt = base64_authorization_header.encode('utf-8')
+            decoded_bytes = base64.b64decode(byt)
+            decoded = decoded_bytes.decode('utf-8')
+            return decoded
+        except (base64.binascii.Error, UnicodeDecodeError):
+            return None
