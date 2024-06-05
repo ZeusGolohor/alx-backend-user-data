f0db7962084b282ab0bca60920bbf0eda0bbb1dd
zeusgolohor <zeusgolohor@gmail.com>
2024-06-05 01:31:31 +0000

    Create a class BasicAuth that inherits from Auth. For the moment this class will be empty.
    Update api/v1/app.py for using BasicAuth class instead of Auth depending of the value of the environment variable AUTH_TYPE, If AUTH_TYPE is equal to basic_auth:
    
    import BasicAuth from api.v1.auth.basic_auth
    create an instance of BasicAuth and assign it to the variable auth
    Otherwise, keep the previous mechanism with auth an instance of Auth.
    
    In the first terminal:

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+A module used to implement basic auth
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    A class used to implement basic auth.
+    """
+    pass
