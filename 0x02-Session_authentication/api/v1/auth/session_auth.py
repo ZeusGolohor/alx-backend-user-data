809453e8144c948f1c4f9f509de8c19f44c55462
zeusgolohor <zeusgolohor@gmail.com>
2024-06-07 02:04:42 +0000

    Update SessionAuth class:
    Create an instance method def current_user(self, request=None): (overload) that returns a User instance based on a cookie value:
    
    You must use self.session_cookie(...) and self.user_id_for_session_id(...) to return the User ID based on the cookie _my_session_id
    By using this User ID, you will be able to retrieve a User instance from the database - you can use User.get(...) for retrieving a User from the database.
    Now, you will be able to get a User based on his session ID.
    
    In the first terminal:

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,6 +4,7 @@ A model to enable session authentication.
 """
 from api.v1.auth.auth import Auth
 import uuid
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -33,3 +34,17 @@ class SessionAuth(Auth):
         if not isinstance(session_id, str):
             return (None)
         return (self.user_id_by_session_id.get(session_id))
+
+    def current_user(self, request=None):
+        """
+        A method used to retreive a user instance via cookies.
+        """
+        if request is None:
+            return (None)
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return (None)
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return (None)
+        return (User.get(user_id))
