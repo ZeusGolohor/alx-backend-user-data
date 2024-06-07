3142d33a513a8b82a42d7ae6f145094781fd7a28
zeusgolohor <zeusgolohor@gmail.com>
2024-06-07 00:44:54 +0000

    Create a class SessionAuth that inherits from Auth. For the moment this class will be empty. It’s the first step for creating a new authentication mechanism:
    validate if everything inherits correctly without any overloading
    validate the “switch” by using environment variables
    Update api/v1/app.py for using SessionAuth instance for the variable auth depending of the value of the environment variable AUTH_TYPE, If AUTH_TYPE is equal to session_auth:
    
    import SessionAuth from api.v1.auth.session_auth
    create an instance of SessionAuth and assign it to the variable auth
    Otherwise, keep the previous mechanism.
    
    In the first terminal:

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+A model to enable session authentication.
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    A class to enable session based authentication.
+    """
+    pass
