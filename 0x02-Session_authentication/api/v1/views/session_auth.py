9499bedf18d4ff610af24f81f876f8ae89eb69c3
zeusgolohor <zeusgolohor@gmail.com>
2024-06-07 02:56:05 +0000

    Create a new Flask view that handles all routes for the Session authentication.
    In the file api/v1/views/session_auth.py, create a route POST /auth_session/login (= POST /api/v1/auth_session/login):
    
    Slash tolerant (/auth_session/login == /auth_session/login/)
    You must use request.form.get() to retrieve email and password parameters
    If email is missing or empty, return the JSON { "error": "email missing" } with the status code 400
    If password is missing or empty, return the JSON { "error": "password missing" } with the status code 400
    Retrieve the User instance based on the email - you must use the class method search of User (same as the one used for the BasicAuth)
    If no User found, return the JSON { "error": "no user found for this email" } with the status code 404
    If the password is not the one of the User found, return the JSON { "error": "wrong password" } with the status code 401 - you must use is_valid_password from the User instance
    Otherwise, create a Session ID for the User ID:
    You must use from api.v1.app import auth - WARNING: please import it only where you need it - not on top of the file (can generate circular import - and break first tasks of this project)
    You must use auth.create_session(..) for creating a Session ID
    Return the dictionary representation of the User - you must use to_json() method from User
    You must set the cookie to the response - you must use the value of the environment variable SESSION_NAME as cookie name - tip
    In the file api/v1/views/__init__.py, you must add this new view at the end of the file.
    
    In the first terminal:

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+Model for session views
+"""
+from api.v1.views import app_views
+from flask import request, jsonify, make_response
+from models.user import User
+import os
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def session_login():
+    """
+    Used to login into the app.
+    """
+    email = request.form.get("email")
+    if (email is None) or (len(email) == 0):
+        error_response = jsonify({"error": "email missing"})
+        return make_response(error_response, 400)
+    password = request.form.get("password")
+    if (password is None) or (len(password) == 0):
+        error_response = jsonify({"error": "password missing"})
+        return make_response(error_response, 400)
+    users = User.search({"email": email})
+    if not users or users == []:
+        return jsonify({"error": "no user found for this email"}), 404
+    for user in users:
+        if user.is_valid_password(password):
+            from api.v1.app import auth
+            session_id = auth.create_session(user.id)
+            res = jsonify(user.to_json())
+            session_name = os.getenv('SESSION_NAME')
+            res.set_cookie(session_name, session_id)
+            return res
+    return jsonify({"error": "wrong password"}), 401
