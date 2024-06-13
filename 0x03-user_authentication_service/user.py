03f27ba571c0773b1b8f0bd4e599fae0bf405d71
golohorzeus <golohorzeusufuoma@gmail.com>
2024-06-13 15:42:41 -0700

    In this task you will create a SQLAlchemy model named User for a database table named users (by using the mapping declaration of SQLAlchemy).
    The model will have the following attributes:
    
    id, the integer primary key
    email, a non-nullable string
    hashed_password, a non-nullable string
    session_id, a nullable string
    reset_token, a nullable string

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -3,7 +3,7 @@
 A script to create a SQLAlchemy model named User
 for a database table named users.
 """
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
 import uuid
 
@@ -15,11 +15,11 @@ class User(Base):
     A class to manage the user model.
     """
     __tablename__ = 'users'
-    id = Column(String(60), primary_key=True)
-    email = Column(String(128), nullable=False)
-    hashed_password = Column(String(128), nullable=False)
-    session_id = Column(String(128))
-    reset_token = Column(String(128))
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
 
     def __init__(self, *args, **kwargs):
         """
