647d6f557ae79a01fbab4e43b24e076aeb529c41
golohorzeus <golohorzeusufuoma@gmail.com>
2024-06-13 18:35:01 -0700

    In this task, you will implement the end-point to register a user. Define a users function that implements the POST /users route.
    Import the Auth object and instantiate it at the root of the module as such:
    
    from auth import Auth
    
    AUTH = Auth()
    The end-point should expect two form data fields: "email" and "password". If the user does not exist, the end-point should register it and respond with the following JSON payload:
    
    {"email": "<registered email>", "message": "user created"}
    If the user is already registered, catch the exception and return a JSON payload of the form
    
    {"message": "email already registered"}
    and return a 400 status code
    
    Remember that you should only use AUTH in this app. DB is a lower abstraction that is proxied by Auth.
    
    Terminal 1:
    
    bob@dylan:~$ python3 app.py
    * Serving Flask app "app" (lazy loading)
     * Environment: production
       WARNING: This is a development server. Do not use it in a production deployment.
       Use a production WSGI server instead.
     * Debug mode: off
     * Running on http://0.0.0.0:5000/ (Press CTRL+C to quit)
    
    Terminal 2:

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,10 +2,14 @@
 """
 A script to handle application routes.
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+from sqlalchemy.orm.exc import NoResultFound
 
 
 app = Flask(__name__)
+AUTH = Auth()
+
 
 @app.route('/')
 def index():
@@ -13,6 +17,24 @@ def index():
     To handle the default route.
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    """
+    A method used to handle users.
+    """
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            user = AUTH._db.find_user_by(email=email)
+            return jsonify({"message": "email already registered"}), 400
+        except NoResultFound:
+            user = AUTH.register_user(email, password)
+            return jsonify({
+                            "email": "{}".format(email),
+                            "message": "user created"})
 
 
 if __name__ == "__main__":
