10117e447355481abcf6d0a4c8b5bacd684b5722
golohorzeus <golohorzeusufuoma@gmail.com>
2024-06-13 18:54:49 -0700

    In this task, you will implement the Auth.valid_login method. It should expect email and password required arguments and return a boolean.
    Try locating the user by email. If it exists, check the password with bcrypt.checkpw. If it matches return True. In any other case, return False.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -40,3 +40,16 @@ class Auth:
                                      email=email,
                                      hashed_password=_hash_password(password))
             return (user)
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+        A method used to valid a user's password.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                return bcrypt.checkpw(
+                                      password.encode('utf-8'),
+                                      user.hashed_password)
+        except NoResultFound:
+            return (False)
