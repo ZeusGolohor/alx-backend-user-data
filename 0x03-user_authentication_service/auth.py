811c6dbb3ec688b847f5f32679b52382a1c6540c
golohorzeus <golohorzeusufuoma@gmail.com>
2024-06-13 18:05:47 -0700

    In this task, you will implement the Auth.register_user in the Auth class provided below: Note that Auth._db is a private property and should NEVER be used from outside the class.
    Auth.register_user should take mandatory email and password string arguments and return a User object.
    
    If a user already exist with the passed email, raise a ValueError with the message User <user's email> already exists.
    
    If not, hash the password with _hash_password, save the user to the database using self._db and return the User object.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,9 @@
 A script to handle user auth.
 """
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
@@ -12,3 +15,28 @@ def _hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hash_pwd = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hash_pwd
+
+
+class Auth:
+    """
+    Auth class to interact with the authentication database.
+    """
+    def __init__(self):
+        """
+        Used to initialize the class.
+        """
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        A method used to register a new user
+        """
+        try:
+            check_user = self._db.find_user_by(email=email)
+            if check_user:
+                raise ValueError('User {} already exists'.format(email))
+        except NoResultFound:
+            user = self._db.add_user(
+                                     email=email,
+                                     hashed_password=_hash_password(password))
+            return (user)
